d8a2f5edac6bda71a6abc722ca78a4cb0173174f
carlivas <carlaskehave@outlook.com>
2024-09-17 17:39:33 +0200

    renamed _chunks files to be the new defaults

--- render.py
+++ render.py
@@ -7,18 +7,31 @@ import pickle
 from plant import Plant
 from rendering import pos_to_screen, r_to_screen, color_to_rgb
 
-file_path = 'Data\states_rep_p1e-1.pkl'
 
-# Load simulation states
-states = pickle.load(
-    open(file_path, 'rb'))
-print(f'Loaded {len(states)} simulation states.')
+def load_metadata(file_path):
+    with open(file_path, 'rb') as file:
+        metadata = pickle.load(file)
+    return metadata
+
+
+def load_states_in_chunks(file_path, chunk_size):
+    with open(file_path, 'rb') as file:
+        while True:
+            try:
+                chunk = pickle.load(file)
+                for i in range(0, len(chunk), chunk_size):
+                    yield chunk[i:i + chunk_size]
+            except EOFError:
+                break
 
 
 def setup():
-    global screen, boundary_scale, frame, fps, clock, qt_show_kwargs
+    global screen, boundary_scale, x_offset, y_offset, speed_move, speed_zoom, frame, fps, clock, qt_show_kwargs
     screen_width, screen_height = 700, 700
     boundary_scale = 0.8
+    x_offset, y_offset = 0, 0
+    speed_move = 50
+    speed_zoom = 0.2
     frame = 0
 
     pygame.init()
@@ -26,7 +39,7 @@ def setup():
 
     screen = pygame.display.set_mode(
         (screen_width, screen_height))
-    fps = 60
+    fps = 30
     clock = pygame.time.Clock()
     qt_show_kwargs = {
         'boundary_scale': 0.8,
@@ -42,13 +55,29 @@ color_background = (235, 235, 205)
 
 def draw_loading_progress():
     screen.fill(color_background)
-    font = pygame.font.Font(None, 36)
+    font = pygame.font.Font(None, 24)
     text = font.render(
-        f'Loading... {total_states} states loaded', True, (255, 255, 255))
+        f'Loaded {last_chunk_loaded} / {n_iter} ({last_chunk_loaded * 100 // n_iter} %) simulation states.', True, (0, 0, 0))
+
+    progress_bar_width = 300
+    progress_bar_height = 20
+    progress = last_chunk_loaded / n_iter
+
+    # Draw the filled portion of the progress bar
+    pygame.draw.rect(screen, (0, 128, 0),
+                     (screen.get_width() / 2 - progress_bar_width / 2,
+                      screen.get_height() / 2 - progress_bar_height / 2,
+                      progress_bar_width * progress, progress_bar_height))
+
+    # Draw the outline of the progress bar
+    pygame.draw.rect(screen, (0, 0, 0),
+                     (screen.get_width() / 2 - progress_bar_width / 2,
+                      screen.get_height() / 2 - progress_bar_height / 2,
+                      progress_bar_width, progress_bar_height), 2)
+
     text_rect = text.get_rect(
-        center=(screen.get_width() / 2, screen.get_height() / 2))
+        center=(screen.get_width() / 2, screen.get_height() / 2 + 25))
     screen.blit(text, text_rect)
-    pygame.display.flip()
 
 
 def draw():
@@ -61,7 +90,8 @@ def draw():
 
     for point in qt.all_points():
         plant = point.data
-        p_screen = pos_to_screen(screen, plant.pos, boundary_scale)
+        p_screen = pos_to_screen(
+            screen, plant.pos, boundary_scale, x_offset, y_offset)
         r_screen = r_to_screen(screen, plant.r, boundary_scale)
         c_screen = plant.color
 
@@ -72,24 +102,69 @@ def draw():
                 screen, p_screen[0], p_screen[1], r_screen, c_screen)
 
 
+surfix = 'temp'
+file_path_metadata = f'Data\metadata_{surfix}.pkl'
+file_path_states = f'Data\states_{surfix}.pkl'
+
+metadata = load_metadata(file_path_metadata)
+
+total_states = n_iter = metadata['n_iter']
+chunk_size = metadata['chunk_size']
+
+# Initialize an empty list to hold all the QuadTree objects
+states = []
+
 # Main loop
 setup()
 
+loading = True
 running = True
 try:
+    while loading:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                loading = False
+                running = False
+        # Load the data in chunks and extend the states list
+        i = 1
+        for chunk in load_states_in_chunks(file_path_states, chunk_size):
+            last_chunk_loaded = min(chunk_size * i, n_iter)
+            states.extend(chunk)
+
+            draw_loading_progress()
+            i += 1
+            pygame.display.flip()
+
+        if last_chunk_loaded == n_iter:
+            loading = False
+            running = True
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                loading = False
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    y_offset += speed_move
+                elif event.key == pygame.K_s:
+                    y_offset -= speed_move
+                elif event.key == pygame.K_a:
+                    x_offset += speed_move
+                elif event.key == pygame.K_d:
+                    x_offset -= speed_move
 
+                if event.key == pygame.K_z:
+                    boundary_scale -= speed_zoom
+                elif event.key == pygame.K_x:
+                    boundary_scale += speed_zoom
         draw()
 
-        pygame.display.flip()
         frame += 1  # Update frame
         frame %= len(states)  # Loop back to the first frame
-
         # Control the frame rate
         clock.tick(fps)
+        pygame.display.flip()
 
 except SystemExit:
     pass
--- simulate.py
+++ simulate.py
@@ -4,44 +4,88 @@ import numpy as np
 import pickle
 import time
 import subprocess
+import sys
+import noise
+
 
 from plant import Plant
 from simulation import Simulation
 import quadT
 
+# Increase the recursion limit
+sys.setrecursionlimit(100000)
+
+
+def save_states_in_chunks(filename, states, chunk_size):
+    l = len(states)
+    with open(filename, 'wb') as f:
+        for i in range(0, len(states), chunk_size):
+            pickle.dump(states[i:i + chunk_size], f)
+            last_chunk_saved = min(i + chunk_size, l)
+            print(
+                f'Saved states {last_chunk_saved}/{l} ({last_chunk_saved * 100 // l} %) to {filename}', end='\r')
+
+
+def save_metadata(metadata, filename):
+    with open(filename, 'wb') as f:
+        pickle.dump(metadata, f)
+    print(f'\nSaved metadata to {filename}')
+
 
 print('\nRunning simulation...')
-filename = f'Data\states_temp.pkl'
+surfix = 'temp'
+file_path_states = f'Data\states_{surfix}.pkl'
+file_path_metadata = f'Data\metadata_{surfix}.pkl'
+
 save_file = True
-render = False
+render = True
 
-
-num_plants = 100
-n_iter = 250
 seed = 0
-
 np.random.seed(seed)
-plant_kwargs = {'r_start': 1/num_plants,
-                'r_max': 5/num_plants,
-                'growth_rate': 0.01/num_plants,
-                'reproduction_chance': 0.0}
+
+num_plants = 10000
+n_iter = 100000
+
+half_width = half_height = 0.5
+A_bound = 2 * half_width * 2 * half_height  # ms2
+
+m2pp = m2_per_plant = 1000  # m2/plant
+_m = meter_to_dimless = np.sqrt(A_bound/(m2pp*num_plants))  # ms/m
+print(f'1 m = {_m} units')
+r_start = 0.01 * _m  # m
+r_max = 30 * _m  # m
+growth_rate = 0.1 * _m  # m horizontal growth per year
+r_chance = 1  # probability of reproduction (besides site quality)
+# Initialize simulation
+
+plant_kwargs = {'r_start': r_start,
+                'r_max': r_max,
+                'growth_rate': growth_rate,
+                'reproduction_chance': r_chance}
 simulation_kwargs = {'r_max_global': plant_kwargs['r_max']}
 
-# Initialize simulation
-center = (0, 0)
-half_width = half_height = 0.5
-simulation = Simulation(
-    quadT.QuadTree(center, half_width, half_height, capacity=4), **simulation_kwargs)
+metadata = {
+    'num_plants': num_plants,
+    'n_iter': n_iter,
+    'seed': seed,
+    'plant_kwargs': plant_kwargs,
+    'simulation_kwargs': simulation_kwargs,
+    'chunk_size': np.clip(n_iter//10, 1, 50)
+}
 
-for i in range(num_plants):
-    rand_pos = np.random.uniform(-0.5, 0.5, 2)
+simulation = Simulation(
+    quadT.QuadTree((0, 0), half_width, half_height, capacity=4), **simulation_kwargs)
+
+i = 0
+while i < num_plants:
+    rand_pos = np.random.uniform(-half_width, half_width, 2)
     this_plant_kwargs = plant_kwargs.copy()
     this_plant_kwargs['r_start'] = np.random.uniform(
-        plant_kwargs['r_start'], plant_kwargs['r_start']*4)
+        plant_kwargs['r_start']*0.8, plant_kwargs['r_start']*1.2)
     plant = Plant(rand_pos, **this_plant_kwargs)
     simulation.add_plant(plant)
-
     print(f'Planted {i+1}/{num_plants} plants', end='\r')
+    i += 1
 
 
 # Run simulation
@@ -61,17 +105,20 @@ except KeyboardInterrupt:
     print('\nSimulation interrupted.')
 
 
-def save_simulation(simulation, filename):
-    with open(filename, 'wb') as f:
-        pickle.dump(simulation.states, f)
-    print('Saved simulation states' + ' '*100)
-
-
 print('\nSimulation finished.')
 if save_file:
     print('Saving results...')
-    save_simulation(simulation, filename=filename)
+    n_iter = len(simulation.states)
+    chunk_size = np.clip(n_iter//10, 1, 50)
+    save_states_in_chunks(file_path_states, simulation.states,
+                          chunk_size=chunk_size)
+    metadata['file_path_states'] = file_path_states
+    metadata['n_iter'] = n_iter
+    metadata['chunk_size'] = chunk_size
+    save_metadata(metadata, file_path_metadata)
 if render:
     print('Rendering results...')
     # Run the rendering.py file
-    subprocess.run(['python', 'render.py'])
+    subprocess.run(['python', 'render_chunks.py'])
+
+print('\nDone!')
