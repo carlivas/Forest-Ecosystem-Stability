f7dbf52c5e3eeeb0371c723d1eb379344f193005
carlivas <carlaskehave@outlook.com>
2024-10-19 04:16:55 +0200

    StateBuffer and DataBuffer classes are working, but support for fields is needed

--- simulation.py
+++ simulation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.spatial import KDTree
 from scipy.interpolate import RegularGridInterpolator
 import copy
@@ -17,19 +18,32 @@ def check_collision(p1, p2):
 
 class Simulation:
     def __init__(self, **kwargs):
+        self.t = 0
         self.plants = []
 
         self.land_quality = kwargs.get('land_quality')
 
         self.half_width = kwargs.get('half_width')
         self.half_height = kwargs.get('half_height', self.half_width)
-        density_check_radius = kwargs.get('density_check_radius')
-        density_check_resolution = kwargs.get('density_check_resolution')
         self.density_field = DensityField(
-            self.half_width, self.half_height, density_check_radius, density_check_resolution)
+            self.half_width,
+            self.half_height,
+            kwargs.get('density_check_radius'),
+            kwargs.get('density_check_resolution')
+        )
 
         self.kt_leafsize = kwargs.get('kt_leafsize')
         self.kt = None
+
+        self.state_buffer = StateBuffer(
+            size=kwargs.get('state_buffer_size', 100),
+            skip=kwargs.get('state_buffer_skip', 1),
+            preset_times=kwargs.get('state_buffer_preset_times', None)
+        )
+
+        self.data_buffer = DataBuffer(
+            size=kwargs.get('n_iter'),
+        )
 
     def add(self, plant):
         if isinstance(plant, Plant):
@@ -67,9 +81,15 @@ class Simulation:
                 new_plants.append(plant)
         self.plants = new_plants
 
-        # Update KDTree and density field
+        # Update nessessary data structures
         self.update_kdtree()
-        self.density_field.update(self)
+        self.update_density_field()
+
+        self.data_buffer.analyze_and_add(self.get_state(), self.t)
+
+        self.t += 1
+        if self.t % self.state_buffer.skip == 0 or self.t in self.state_buffer.preset_times:
+            self.state_buffer.add(self.get_state(), t=self.t)
 
     def get_collisions(self, plant):
         plant.is_colliding = False
@@ -98,3 +118,199 @@ class Simulation:
 
     def get_density_field(self):
         return copy.deepcopy(self.density_field)
+
+    def initiate_uniform_lifetimes(self, n, t_min, t_max, **plant_kwargs):
+        growth_rate = plant_kwargs['growth_rate']
+        plant_kwargs['r_max'] = t_max * growth_rate
+        plants = [
+            Plant(
+                pos=np.random.uniform(-self.half_width, self.half_width, 2),
+                r=np.random.uniform(t_min, t_max) * growth_rate,
+                **plant_kwargs
+            )
+            for _ in range(n)
+        ]
+        self.add(plants)
+        self.update_kdtree()
+        self.update_density_field()
+        # self.state_buffer.append(self.get_state())
+        self.state_buffer.add(self.get_state(), t=0)
+
+    # def initiate(self, n, **plant_kwargs):
+    #     d = 2
+    #     dist_min = plant_kwargs['r_min']**d
+    #     dist_max = plant_kwargs['r_max']**d
+    #     plants = [
+    #         Plant(
+    #             pos=np.random.uniform(-half_width, half_width, 2),
+    #             r=np.random.uniform(dist_min, dist_max)**(1/d),
+    #             **plant_kwargs
+    #         )
+    #         for _ in range(n)
+    #     ]
+
+    def plot(self, size=6):
+        plot_state(self.get_state(), size)
+        return fig, ax
+
+    def plot_state(self, state, t=None, size=6, fig=None, ax=None):
+        if ax is None:
+            fig, ax = plt.subplots(1, 1, figsize=(size, size))
+        # ax.set_title('State')
+        # ax.set_xlabel('Width (u)')
+        # ax.set_ylabel('Height (u)')
+        ax.set_xlim(-self.half_width, self.half_width)
+        ax.set_ylim(-self.half_height, self.half_height)
+        ax.set_aspect('equal', 'box')
+        for plant in state:
+            if t is not None:
+                ax.set_title(f't = {t}', fontsize=7)
+            ax.add_artist(plt.Circle(plant.pos, plant.r,
+                          color='green', fill=True, transform=ax.transData))
+
+        ax.set_xticks([])
+        ax.set_yticks([])
+        return fig, ax
+
+    def plot_states(self, states, times=None, size=6):
+        l = len(states)
+        n_rows = int(np.floor(l / np.sqrt(l)))
+        n_cols = (l + 1) // n_rows + (l % n_rows > 0)
+        print(f'simulation: {n_rows = }, {n_cols = }')
+
+        fig, ax = plt.subplots(
+            n_rows, n_cols, figsize=(size*n_cols, size*n_rows))
+        fig.tight_layout()
+        if len(states) == 1:
+            self.plot_state(states[0], t=times[0], size=size, ax=ax)
+        else:
+            i = 0
+            # for i, state in enumerate(states):
+            while i < len(states):
+                state = states[i]
+                if n_rows == 1:
+                    k = i
+                    self.plot_state(
+                        state=state, t=times[i], size=size, fig=fig, ax=ax[k])
+                else:
+                    l = i//n_cols
+                    k = i % n_cols
+                    self.plot_state(
+                        state=state, t=times[i], size=size, fig=fig, ax=ax[l, k])
+
+                i += 1
+        if len(states) < n_rows*n_cols:
+            for j in range(len(states), n_rows*n_cols):
+                if n_rows == 1:
+                    ax[j].axis('off')
+                else:
+                    l = j//n_cols
+                    k = j % n_cols
+                    ax[l, k].axis('off')
+
+        return fig, ax
+
+
+class StateBuffer:
+    def __init__(self, size, skip=1, preset_times=None):
+        self.size = size
+        self.skip = skip
+        self.states = []
+        self.times = []
+        self.length = 0
+
+        self.start_time = 0
+        self.preset_times = preset_times
+
+    def add(self, state, t):
+        if len(self.states) == 0 or state != self.states[-1]:
+            self.states.append(state)
+            self.times.append(t)
+            if len(self.states) > self.size:
+                self.states.pop(0)
+                self.start_time = self.start_time + self.skip
+            print(
+                f'\nStateBuffer: Added state at time {t}.')
+            self.length = len(self.states)
+
+    def get(self, times=None):
+        if times is None:
+            return copy.deepcopy(self.states)
+
+        indices = []
+        for t in times:
+            if t < self.start_time or t >= self.start_time + self.length:
+                print(
+                    f'!Warning! StateBuffer: Time {t} is out of bounds. Start time: {self.start_time}, End time: {self.start_time + self.length}')
+                indices.append(np.nan)
+            else:
+                indices.append(t - self.start_time)
+
+        print('StateBuffer: buffer_start_time = ', self.start_time)
+        print(f'{indices = }')
+
+        return copy.deepcopy([self.states[i] for i in indices if not np.isnan(i)])
+
+    def get_states(self):
+        return copy.deepcopy(self.states)
+
+    def get_times(self):
+        return copy.deepcopy(self.times)
+
+
+class DataBuffer:
+    def __init__(self, size):
+        self.size = size
+        # self.times = np.arange(size)
+        # self.buffer = np.zeros((size, 2))
+        self.buffer = np.full((size, 2), np.nan)
+        self.times = np.full(size, np.nan)
+        self.length = 0
+
+    def add(self, data, time):
+        self.buffer[time] = data
+        self.times[time] = time
+
+        if len(self.buffer) > self.size:
+            self.buffer.pop(0)
+        self.length = self.length + 1
+
+    def analyze_state(self, state, time):
+        biomass = sum([plant.area for plant in state])
+        population_size = len(state)
+        data = np.array([biomass, population_size])
+        print(' '*40 +
+              f'DataBuffer: {time = }     |     P = {population_size}     |     B = {np.round(biomass, 5)}', end='\r')
+        self.add(data, time)
+        return data
+
+    def analyze_and_add(self, state, time):
+        data = self.analyze_state(state, time)
+        self.add(data, time)
+        return data
+
+    def finalize(self):
+        print(self.length)
+        self.buffer = self.buffer[:self.length]
+        self.times = self.times[:self.length]
+
+    def get_data(self, indices=None):
+
+        if indices is None:
+            return copy.deepcopy(self.buffer)
+        return copy.deepcopy([self.buffer[i] for i in indices])
+
+    def plot(self, size=6):
+        fig, ax = plt.subplots(2, 1, figsize=(size, size))
+        fig.tight_layout()
+        ax[0].plot(self.times, self.buffer[:, 0],
+                   label='Biomass', color='green')
+        # ax[0].set_xticks([])
+        ax[1].plot(self.times, self.buffer[:, 1],
+                   label='Population Size', color='teal')
+        ax[1].set_xlabel('Time')
+
+        for ax_i in ax:
+            ax_i.grid()
+            ax_i.legend()
+        return fig, ax
--- testing.py
+++ testing.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
+import time
 
 from plant import Plant
 from simulation import Simulation
@@ -10,19 +11,14 @@ np.random.seed(seed)
 
 num_plants = 10_000
 n_iter = 100_000
+m2pp = m2_per_plant = 250  # m2/plant
 
 half_width = half_height = 0.5
 A_bound = 2 * half_width * 2 * half_height
-
-m2pp = m2_per_plant = 250  # m2/plant
 _m = np.sqrt(A_bound/(m2pp*num_plants))
 
-print(f'{seed = }')
-print(f'1 m = {_m} u')
-print(f'1 u = {1/_m} m')
-
-sgc = 0.17 + (np.random.rand() - 0.5) * 0.1
-print(f'species_germination_chance = {sgc}')
+sgc = 0.12124776088109324
+# sgc = 0.17 + (np.random.rand() - 0.5) * 0.1
 
 # Initialize simulation
 plant_kwargs = {
@@ -35,183 +31,205 @@ plant_kwargs = {
 
 sim_kwargs = {
     'seed': seed,
+    'n_iter': n_iter,
     'half_width': half_width,
     'half_height': half_height,
     'num_plants': num_plants,
-    'kt_leafsize': 10,
     'land_quality': -0.1,
     'density_check_radius': 100 * _m,
     'density_check_resolution': 25,
+    'kt_leafsize': 10,
+    'state_buffer_size': 25,
+    'state_buffer_skip': 250,
+    'state_buffer_preset_times': [1, 10, 100, 1000, 5000],
 }
 
+print(f'{seed = }')
+print(f'1 m = {_m} u')
+print(f'1 u = {1/_m} m')
+print(f'species_germination_chance = {sgc}')
+print(f'land_quality = {sim_kwargs["land_quality"]}')
+
+
 sim = Simulation(**sim_kwargs)
-
-d = 2
-dist_min = plant_kwargs['r_min']**d
-dist_max = plant_kwargs['r_max']**d
-plants = [
-    Plant(
-        pos=np.random.uniform(-half_width, half_width, 2),
-        # r=plant_kwargs['r_min'],
-        r=np.random.uniform(dist_min, dist_max)**(1/d),
-        **plant_kwargs
-    )
-    for _ in range(num_plants)
-]
+sim.initiate_uniform_lifetimes(
+    n=num_plants, t_min=1, t_max=300, **plant_kwargs)
 
 
-def biomass(state):
-    return sum([plant.area for plant in state])
+start_time = time.time()
+for _ in range(1, n_iter):
+    sim.step()
+    if len(sim.plants) == 0:
+        break
+    elapsed_time = time.time() - start_time
+    print('Simulating... Elapsed time: {:.2f}s'.format(elapsed_time))
+sim.data_buffer.finalize()
+end_time = time.time()
+
+print('\nSimulation complete!' + ' '*20)
+state_buffer_length = sim.state_buffer.length
+print(f'{state_buffer_length = }')
 
 
-ΔQ_L = -1e-5
+fetched_states = sim.state_buffer.get_states()
+fetched_times = sim.state_buffer.get_times()
+sim.plot_states(states=fetched_states, times=fetched_times, size=2)
 
-states = []
-density_field_arr = []
-biomass_arr = []
-num_arr = []
-land_quality_arr = []
-
-sim.add(plants)
-sim.update_kdtree()
-sim.update_density_field()
-state = sim.get_state()
-density_field = sim.get_density_field()
-l = len(state)
-b = biomass(state)
-
-states.append(state)
-density_field_arr.append(density_field.get_values())
-biomass_arr.append(b)
-num_arr.append(l)
-land_quality_arr.append(sim.land_quality)
-
-save_skip = 100
-
-# simulation loop
-try:
-    i = 1
-    while i < n_iter and l > 0:
-        # Q_L = sim.land_quality + ΔQ_L
-        # sim.land_quality = Q_L
-        sim.step()
-        state = sim.get_state()
-        density_field = sim.get_density_field()
-        l = len(state)
-        b = biomass(state)
-
-        if i % save_skip == 0:
-            states.append(state)
-            density_field_arr.append(density_field.get_values())
-
-        biomass_arr.append(b)
-        num_arr.append(l)
-
-        # land_quality_arr.append(Q_L)
-
-        string = f'Iter {i+1:<5}  |  (P, B) = ({l:>6}, {np.round(b, 4):>6})'
-        Δ_num = num_arr[-1] - num_arr[-2]
-        Δ_bm = biomass_arr[-1] - biomass_arr[-2]
-        string += f'  |  (ΔP, ΔB) = ({Δ_num:>4}, {np.round(Δ_bm, 4):>6})'
-        # string += f'  |  Q_L = {np.round(Q_L, 3):>6}'
-        print(string + ' '*20, end='\r')
-        i += 1
-        if l == 0:
-            break
-except KeyboardInterrupt:
-    print('\nInterrupted by user...')
-
-fig, ax = plt.subplots(2, 1, figsize=(6, 6), sharex=True)
-# ρ = 500  # kg/m3
-# biomass_arr = np.array(biomass_arr) * 1*_m / (ρ * _m**3)
-ax[0].plot(biomass_arr[1:], label='Biomass', color='blue')
-# ax[0].set_title('Biomass and density over time', fontsize=10)
-# ax[0].set_xlabel('Iteration')
-# ax[0].set_ylabel('Biomass', fontsize=8)  # [$\mathrm{kg}$]
-ax[0].grid(True)
-ax[0].legend(fontsize=8)
-
-# num_arr = np.array(num_arr)/(A_bound/_m**2)
-ax[1].plot(num_arr[1:], label='Density', color='red')
-# ax[1].set_title('Density over time')
-ax[1].set_xlabel('Iteration')
-# ax[1].set_ylabel('Density', fontsize=8)  # [plants/$\mathrm{m}^2$]
-ax[1].grid(True)
-ax[1].legend(fontsize=8)
-
-# ax[2].plot(land_quality_arr[:1], label='Land quality', color='green')
-# # ax[2].set_title('Land Quality over time')
-# ax[2].set_xlabel('Iteration', fontsize=8)
-# # ax[2].set_ylabel('Land Quality', fontsize=8)
-# ax[2].legend(fontsize=8)
-
-fig.tight_layout()
-fig.subplots_adjust(hspace=0)
+sim.data_buffer.plot()
 plt.show()
 
-plt.figure()
-plt.title('log(Density) over log(Biomass)')
-plt.plot(num_arr, biomass_arr)
-plt.xscale('log')
-plt.yscale('log')
-plt.xlabel('Density')
-plt.ylabel('Biomass')
-plt.show()
-
-fig, ax = plt.subplots(1, 2, figsize=(10, 5))
-Q_L = sim_kwargs['land_quality']
+# def biomass(state):
+#     return sum([plant.area for plant in state])
 
 
-def update(frame):
-    print(f'Rendering frame {frame+1}/{len(states)}...', end='\r')
+# ΔQ_L = -1e-5
 
-    ax[0].clear()
-    ax[1].clear()
+# states = []
+# density_field_arr = []
+# biomass_arr = []
+# num_arr = []
+# land_quality_arr = []
 
-    sizes = []
+# sim.add(plants)
+# sim.update_kdtree()
+# sim.update_density_field()
+# state = sim.get_state()
+# density_field = sim.get_density_field()
+# l = len(state)
+# b = biomass(state)
 
-    ax[0].set_xlim(-half_width, half_width)
-    ax[0].set_ylim(-half_height, half_height)
-    ax[0].set_aspect('equal', 'box')
-    ax[0].set_xlabel('Width (u)')
-    ax[0].set_ylabel('Height (u)')
-    for plant in states[frame]:
-        sizes.append(plant.r)
-        circle = plt.Circle(plant.pos, plant.r, color='green',
-                            fill=True, transform=ax[0].transData)
-        ax[0].add_patch(circle)
+# states.append(state)
+# density_field_arr.append(density_field.get_values())
+# biomass_arr.append(b)
+# num_arr.append(l)
+# land_quality_arr.append(sim.land_quality)
 
-    # ax[1].set_xlabel('Size (u)')
-    # ax[1].set_ylabel('Frequency')
-    # ax[1].set_xlim(0, 1.2*plant_kwargs['r_max'])
-    # ax[1].set_ylim(0, num_plants)
+# save_skip = 100
 
-    # bins = np.linspace(0, 1.2*plant_kwargs['r_max'], 25)
-    # size = np.array(sizes)
-    # ax[1].hist(sizes, bins=bins, color='black')
+# # simulation loop
+# try:
+#     i = 1
+#     while i < n_iter and l > 0:
+#         # Q_L = sim.land_quality + ΔQ_L
+#         # sim.land_quality = Q_L
+#         sim.step()
+#         state = sim.get_state()
+#         density_field = sim.get_density_field()
+#         l = len(state)
+#         b = biomass(state)
 
-    # fig.suptitle(f'Plant and size distribution (iteration {frame*save_skip})')
+#         if i % save_skip == 0:
+#             states.append(state)
+#             density_field_arr.append(density_field.get_values())
 
-    ax[1].set_xlim(-half_width, half_width)
-    ax[1].set_ylim(-half_height, half_height)
-    ax[1].set_aspect('equal', 'box')
-    ax[1].set_xlabel('Width (u)')
-    ax[1].set_ylabel('Height (u)')
+#         biomass_arr.append(b)
+#         num_arr.append(l)
 
-    xx = np.linspace(-half_width, half_width, sim.density_field.shape[0])
-    yy = np.linspace(-half_height, half_height, sim.density_field.shape[1])
-    X, Y = np.meshgrid(xx, yy)
-    contour = ax[1].contourf(X, Y, density_field_arr[frame].T + Q_L,
-                             cmap='viridis', levels=50)
+#         # land_quality_arr.append(Q_L)
 
-    fig.suptitle(
-        f'Plant distribution and reproduction field (Iteration {frame*save_skip})')
+#         string = f'Iter {i+1:<5}  |  (P, B) = ({l:>6}, {np.round(b, 4):>6})'
+#         Δ_num = num_arr[-1] - num_arr[-2]
+#         Δ_bm = biomass_arr[-1] - biomass_arr[-2]
+#         string += f'  |  (ΔP, ΔB) = ({Δ_num:>4}, {np.round(Δ_bm, 4):>6})'
+#         # string += f'  |  Q_L = {np.round(Q_L, 3):>6}'
+#         print(string + ' '*20, end='\r')
+#         i += 1
+#         if l == 0:
+#             break
+# except KeyboardInterrupt:
+#     print('\nInterrupted by user...')
+
+# fig, ax = plt.subplots(2, 1, figsize=(6, 6), sharex=True)
+# # ρ = 500  # kg/m3
+# # biomass_arr = np.array(biomass_arr) * 1*_m / (ρ * _m**3)
+# ax[0].plot(biomass_arr[1:], label='Biomass', color='blue')
+# # ax[0].set_title('Biomass and density over time', fontsize=10)
+# # ax[0].set_xlabel('Iteration')
+# # ax[0].set_ylabel('Biomass', fontsize=8)  # [$\mathrm{kg}$]
+# ax[0].grid(True)
+# ax[0].legend(fontsize=8)
+
+# # num_arr = np.array(num_arr)/(A_bound/_m**2)
+# ax[1].plot(num_arr[1:], label='Density', color='red')
+# # ax[1].set_title('Density over time')
+# ax[1].set_xlabel('Iteration')
+# # ax[1].set_ylabel('Density', fontsize=8)  # [plants/$\mathrm{m}^2$]
+# ax[1].grid(True)
+# ax[1].legend(fontsize=8)
+
+# # ax[2].plot(land_quality_arr[:1], label='Land quality', color='green')
+# # # ax[2].set_title('Land Quality over time')
+# # ax[2].set_xlabel('Iteration', fontsize=8)
+# # # ax[2].set_ylabel('Land Quality', fontsize=8)
+# # ax[2].legend(fontsize=8)
+
+# fig.tight_layout()
+# fig.subplots_adjust(hspace=0)
+# plt.show()
+
+# plt.figure()
+# plt.title('log(Density) over log(Biomass)')
+# plt.plot(num_arr, biomass_arr)
+# plt.xscale('log')
+# plt.yscale('log')
+# plt.xlabel('Density')
+# plt.ylabel('Biomass')
+# plt.show()
+
+# fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+# Q_L = sim_kwargs['land_quality']
 
 
-ani = animation.FuncAnimation(fig, update, frames=len(
-    states), repeat=True, interval=300)
+# def update(frame):
+#     print(f'Rendering frame {frame+1}/{len(states)}...', end='\r')
+
+#     ax[0].clear()
+#     ax[1].clear()
+
+#     sizes = []
+
+#     ax[0].set_xlim(-half_width, half_width)
+#     ax[0].set_ylim(-half_height, half_height)
+#     ax[0].set_aspect('equal', 'box')
+#     ax[0].set_xlabel('Width (u)')
+#     ax[0].set_ylabel('Height (u)')
+#     for plant in states[frame]:
+#         sizes.append(plant.r)
+#         circle = plt.Circle(plant.pos, plant.r, color='green',
+#                             fill=True, transform=ax[0].transData)
+#         ax[0].add_patch(circle)
+
+#     # ax[1].set_xlabel('Size (u)')
+#     # ax[1].set_ylabel('Frequency')
+#     # ax[1].set_xlim(0, 1.2*plant_kwargs['r_max'])
+#     # ax[1].set_ylim(0, num_plants)
+
+#     # bins = np.linspace(0, 1.2*plant_kwargs['r_max'], 25)
+#     # size = np.array(sizes)
+#     # ax[1].hist(sizes, bins=bins, color='black')
+
+#     # fig.suptitle(f'Plant and size distribution (iteration {frame*save_skip})')
+
+#     ax[1].set_xlim(-half_width, half_width)
+#     ax[1].set_ylim(-half_height, half_height)
+#     ax[1].set_aspect('equal', 'box')
+#     ax[1].set_xlabel('Width (u)')
+#     ax[1].set_ylabel('Height (u)')
+
+#     xx = np.linspace(-half_width, half_width, sim.density_field.shape[0])
+#     yy = np.linspace(-half_height, half_height, sim.density_field.shape[1])
+#     X, Y = np.meshgrid(xx, yy)
+#     contour = ax[1].contourf(X, Y, density_field_arr[frame].T + Q_L,
+#                              cmap='viridis', levels=50)
+
+#     fig.suptitle(
+#         f'Plant distribution and reproduction field (Iteration {frame*save_skip})')
 
 
-print('\nSaving animation...')
-ani.save('plant_simulation.mp4', writer='ffmpeg', fps=2)
-print('\nDone!')
+# ani = animation.FuncAnimation(fig, update, frames=len(
+#     states), repeat=True, interval=300)
+
+
+# print('\nSaving animation...')
+# ani.save('plant_simulation.mp4', writer='ffmpeg', fps=2)
+# print('\nDone!')
