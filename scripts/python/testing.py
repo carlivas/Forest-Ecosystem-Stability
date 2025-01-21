b2c0fb4a8ab9d948fcda7d6705a5c52f3b4a1024
Carl Ivarsen Askehave <carlaskehave@outlook.com>
2025-01-21 13:53:26 +0100

    Revert "Slight modification from modi"

--- scripts/python/testing.py
+++ scripts/python/testing.py
@@ -8,36 +8,32 @@ from mods.plant import Plant
 from mods.simulation import Simulation, _m_from_m2pp, _m_from_domain_sides, save_simulation_results, plot_simulation_results
 from mods.utilities import save_kwargs, print_nested_dict, convert_to_serializable
 from mods.buffers import StateBuffer, DataBuffer, FieldBuffer, HistogramBuffer
-import shutil
-import os
 
 save_results = True
 plot_results = True
 
-T = 100_000
-num_plants = 3000
+T = 100
+num_plants = 1000
 
 seed = np.random.randint(0, 1_000_000_000)
 np.random.seed(seed)
 kwargs = {
-    'L': 4500,
+    'L': 2500,
     'T': T,
     'precipitation': 6500e-5,
     'seed': seed,
 }
 
-surfix = str(seed)
-folder = f'../../Data/starting_contenders'
+surfix = 'test2'
+folder = f'Data/temp/new_buffers_test'
 
+print(seed)
 sim = Simulation(folder=folder, alias=surfix, **kwargs)
-sim.initiate_uniform_radii(n=num_plants, r_min=sim.r_min/sim._m, r_max=sim.r_max/sim._m)
+print(sim.seed)
+# sim.initiate_uniform_radii(n=num_plants, r_min=0.1, r_max=30)
 sim.run(T=T)
-
-
-figs, axs = sim.plot_buffers()
-for i, fig in enumerate(figs):
-    fig.savefig(f'{folder}/figures/fig_{seed}_{i}.png', dpi=600)
-# plt.show()
+sim.plot_buffers()
+plt.show()
 
 
 
