- Fixed a bug in Simulation.attempt_spawn() where it would bypass the germination probability calculation. - Wrote implementation for periodic boundaries in Notes.ipynb (YET TO IMPLEMENT IN SIMULATION) - Implemented StateBuffer.get_specific_data() function - Implemented functionality in files.py to support get_specific_data() - Changes and creation of new analysis files. - Created files-removing and renaming files. - Renamed and created new testing files.

scripts/python/analysis-correlation.py
@@ -30,104 +30,107 @@ def calculate_fit_and_p_value(fit_func, x, y):
     
     return y_fit, p_value
 
-folder = 'Data/linear_precipitation/L1000'
+folder = 'D:/linear_precipitation/L2000'
 key = 'Biomass'
 save_fig = True
 
-if not os.path.exists(folder):
-    raise FileNotFoundError(f"The specified path does not exist: {folder}")
-
 load_folder = os.path.abspath(folder)
 print(f'load_folder: {load_folder}')
+files = os.listdir(load_folder)
+aliases = [f.split('-')[-1].split('.')[0] for f in files if 'data_buffer-' in f]
+aliases = [s for s in aliases if 'checkpoint' not in s]
+print(f'aliases: {aliases}')
 
-for root, dirs, files in os.walk(load_folder):
-    aliases = [f.split('-')[-1].split('.')[0] for f in files if 'kwargs-' in f]
-    aliases = [s for s in aliases if 'checkpoint' not in s]
-    # aliases = sorted(aliases, key=lambda x: int(x.split('-')[-1]))
-    if not aliases:
-        continue
-    print(f'aliases: {aliases}')
-    
-    max_num_plants = 0
-    kwargs_list = []
-    data_buffer_list = []
-    for i, alias in enumerate(aliases):
-        kwargs = pd.read_json(
-            f'{root}/kwargs-{alias}.json', typ='series').to_dict()
-        data_buffer = pd.read_csv(f'{root}/data_buffer-{alias}.csv')
-        data_buffer_list.append(data_buffer)
-        kwargs_list.append(kwargs)
-    
-    time = pd.concat([data_buffer['Time'] for data_buffer in data_buffer_list], axis=0).reset_index(drop=True)
-    data = pd.concat([data_buffer[key] for data_buffer in data_buffer_list], axis=0).reset_index(drop=True)
-    precipitation = pd.concat([data_buffer['Precipitation'] for data_buffer in data_buffer_list], axis=0).reset_index(drop=True)
+max_num_plants = 0
+kwargs_list = []
+data_buffer_list = []
+for i, alias in enumerate(aliases):
+    kwargs = pd.read_json(
+        f'{load_folder}/kwargs-{alias}.json', typ='series').to_dict()
+    data_buffer = pd.read_csv(f'{load_folder}/data_buffer-{alias}.csv')
+
+    time = data_buffer['Time']
+    data = data_buffer[key]
+    precipitation = data_buffer['Precipitation']
     time_prec = precipitation.index
 
-    time_range = (1000, 27500)
+    time_range = (1000, time_prec.max())
     time_slice = slice(*time_range)
-    
-    window_size = 1000
-    step = window_size//2
-    
+
+    window_size = (time_prec.max() - time_prec.min()) // 15
+    step = window_size//4
+
     data_analysis = data.iloc[time_slice].dropna()
-    
+
     # # Detrend data
-    # data_analysis = data_analysis - data_analysis.rolling(window=window_size, center=True).mean()
-    
+    data_analysis = data_analysis - data_analysis.rolling(window=window_size, center=True).mean()
     time_analysis = time.iloc[data_analysis.index]
-    
+
     data_var = data_analysis.rolling(window=window_size, step=step, center=True).var().dropna()
     time_var = time.iloc[data_var.index]
-    data_var_fit, p_value_var = calculate_fit_and_p_value(lin_func, time_var, data_var)
+    # data_var_fit, p_value_var = calculate_fit_and_p_value(lin_func, time_var, data_var)
 
     data_ac = data_analysis.rolling(window=window_size, step=step, center=True).apply(lambda x: x.autocorr(lag=1)).dropna()
     time_ac = time.iloc[data_ac.index]
-    data_ac_fit, p_value_ac = calculate_fit_and_p_value(lin_func, time_ac, data_ac)
+    # data_ac_fit, p_value_ac = calculate_fit_and_p_value(lin_func, time_ac, data_ac)
 
 
 
-    fig, ax = plt.subplots(2, 2, figsize=(12, 4), sharex=True)
+    fig, ax = plt.subplots(4, 1, figsize=(8, 8), sharex=True)
     ax = ax.T.flatten()
-    ax[0].plot(time, data, color='tab:blue')
-    ax[0].set_ylabel(key)
-
-    ax[1].plot(time_prec, precipitation, color='tab:green')
-    ax[1].set_ylabel('Precipitation')
-
-    ax[2].plot(time_var, data_var_fit, color='black', label=f'p-value: {p_value_var:.2e}')
-    ax[2].plot(time_var, data_var, color='tab:red', marker='o', markersize=5, ls='', markerfacecolor='none')
-    ax[2].set_ylabel(key + ' Variance')
-    ax[2].set_xlabel('Time')
     
+    ax0 = ax[0]
+    ax1 = ax0.twinx()
+    lns1 = ax0.plot(time, data, color='tab:blue', label=key)
+    ax0.set_ylabel(key)
+    lns2 = ax1.plot(time_prec, precipitation, color='k', lw=2, ls='--', label='Precipitation')
+    ax1.set_ylabel('Precipitation')
+    lns = lns1 + lns2
+    labs = [l.get_label() for l in lns]
+    ax0.legend(lns, labs, loc='lower left')
+    ax0.set_ylim(-data.max()*0.1, data.max()*1.1)
+    ax1.set_ylim(-precipitation.max()*0.1, precipitation.max()*1.1)
+    
+    ax[1].plot(time_analysis, data_analysis, color='tab:blue', ls='-', markerfacecolor='white')
+    ax[1].set_ylabel(f'Detrended {key}')
+
+    # ax[2].plot(time_var, data_var_fit, color='black', label=f'p-value: {p_value_var:.2e}')
+    ax[2].plot(time_var, data_var, color='tab:red', marker='o', markersize=6, ls='-', markerfacecolor='white')
+    ax[2].set_ylabel('Variance')
+    ax[2].set_xlabel('Time')
+
     xlim = np.array(ax[2].get_xlim())
     ylim = np.array(ax[2].get_ylim())
-    xOff = xlim.min() + (xlim.max() - xlim.min()) * 0.05
+    # xOff = xlim.min() + (xlim.max() - xlim.min()) * 0.05
     yOff = ylim.min() + (ylim.max() - ylim.min()) * 0.8
+    xOff = time_range[0] + window_size
     ax[2].errorbar(xOff, yOff, xerr=window_size, fmt='', color='black', ecolor='black', capsize=5, capthick=1.5)
-    ax[2].legend()
+    # ax[2].legend()
 
-    ax[3].plot(time_ac, data_ac_fit, color='black', label=f'p-value: {p_value_ac:.2e}')
-    ax[3].plot(time_ac, data_ac, color='tab:purple', marker='o', markersize=5, ls='', markerfacecolor='none')
+    # ax[3].plot(time_ac, data_ac_fit, color='black', label=f'p-value: {p_value_ac:.2e}')
+    ax[3].plot(time_ac, data_ac, color='tab:purple', marker='o', markersize=6, ls='-', markerfacecolor='white')
     ax[3].set_ylabel('Autocorrelation')
     ax[3].set_xlabel('Time')
-    
+
     xlim = np.array(ax[3].get_xlim())
     ylim = np.array(ax[3].get_ylim())
-    xOff = xlim.min() + (xlim.max() - xlim.min()) * 0.05
+    # xOff = xlim.min() + (xlim.max() - xlim.min()) * 0.05
     yOff = ylim.min() + (ylim.max() - ylim.min()) * 0.8
+    xOff = time_range[0] + window_size
     ax[3].errorbar(xOff, yOff, xerr=window_size, fmt='', color='black', ecolor='black', capsize=5, capthick=1.5)
-    ax[3].legend()
-    
+    # ax[3].legend()
+
     for a in ax:
         a.axvline(time_range[0], color='black', ls='--', alpha = 0.5)
         a.axvline(time_range[1], color='black', ls='--', alpha = 0.5)
 
         
-    fig.suptitle(f'{key} Correlation {alias}')
+    fig.suptitle(f'{key.lower()}_correlation_{alias}')
+    fig.subplots_adjust(wspace=0.0, hspace=0.3)
     fig.tight_layout()
-    
+
     if save_fig:
-        fig_path = f'{root}/figures/_{key.lower()}_correlation_{alias}.png'
+        fig_path = f'{load_folder}/figures/{key.lower()}_correlation_{alias}.png'
         fig.savefig(fig_path, dpi=600)
 plt.show()
-    
+

scripts/python/mods/buffers.py
@@ -8,8 +8,9 @@ import copy
 import warnings
 import json
 
-from mods.files import move_to_previous_line
+from mods.files import *
 from mods.plant import Plant
+from io import StringIO
 
 modi_path_kwargs = '../../default_kwargs.json'
 local_path_kwargs = 'default_kwargs.json'
@@ -144,7 +145,7 @@ class DataBuffer:
 
     @staticmethod
     def plot(data, size=6, title='', keys=None, drop_first = True):
-        title = 'DataBuffer ' + title
+        title = 'Data Buffer ' + title
 
         # Specify which keys need to be plotted
         if keys is None:
@@ -268,17 +269,28 @@ class StateBuffer:
         data = pd.DataFrame(columns=self.columns)
         
         if os.path.exists(self.file_path):
-            # print(f'StateBuffer.get_data(): Succesfully conected to {self.file_path}.')
             data = pd.read_csv(self.file_path)
-            if 'id' not in data.keys():
-                data = rewrite_state_buffer_data(data)
-                self.override_data(data)
             print(f'StateBuffer.get_data(): {data.shape[0]} rows loaded.')
         else:
             warnings.warn(
                 f'StateBuffer.get_data(): File {self.file_path} does not exist.')    
 
         return data # returns an empty dataframe if the file does not exist
+    
+    def get_specific_data(self, t):
+        if not isinstance(t, (list, np.ndarray, pd.Series)):
+            t = [t]
+        
+        print(f'StateBuffer.get_specific_data(): Searching for times {t = }...')
+        with open(self.file_path, 'rb') as f:
+            lines = find_all_lines_key_values(f, 't', t, assume_sorted=True)
+            
+            if not lines:
+                return pd.DataFrame(columns=self.columns)
+            
+            data = pd.read_csv(StringIO('\n'.join(lines)), header=None, names=self.columns)
+                
+        return data
 
     def get_last_state(self):     
         # if the file does not exist, return an empty dataframe
@@ -378,7 +390,7 @@ class StateBuffer:
 
     @staticmethod
     def plot(data, size=2, title='', fast=False, n_plots=20):
-        title = 'StateBuffer ' + title
+        title = 'State Buffer ' + title
         if data.empty:
             print('StateBuffer.plot(): No data to plot.')
             fig, ax = plt.subplots(1, 1, figsize=(size, size))
@@ -570,7 +582,7 @@ class FieldBuffer:
 
     @staticmethod
     def plot(data, size=2, n_plots=20, vmin=0, vmax=None, title='', extent=[-0.5, 0.5, -0.5, 0.5]):
-        title = 'FieldBuffer ' + title
+        title = 'Field Buffer ' + title
         if data.empty:
             print('FieldBuffer.plot(): No data to plot.')
             fig, ax = plt.subplots(1, 1, figsize=(size, size))
@@ -714,7 +726,7 @@ class HistogramBuffer:
 
     @staticmethod
     def plot(data, size=2, t=None, nplots=20, title='', density=False, xscale=1, xlabel='', ylabel='frequency'):
-        title = 'HistogramBuffer ' + title
+        title = 'Histogram Buffer ' + title
         start = self.start * xscale
         end = self.end * xscale
         xx = np.linspace(start, end, self.bins)

scripts/python/mods/simulation.py
@@ -158,13 +158,10 @@ class Simulation:
         converted_dict = convert_dict(
             d=self.__dict__, conversion_factors=self.conversion_factors_default, reverse=False)
         self.__dict__.update(converted_dict)
-        self.initiate()
-
-    def initiate(self):
         self.update_kdtree(self.plants)
         self.density_field.update(self.plants)
-        print(
-            f'\nSimulation.initiate(): Time: {time.strftime("%Y-%m-%d %H:%M:%S")}')
+        print(f'Simulation.__init__(): Time: {time.strftime("%H:%M:%S")}')
+        print(f'Simulation.__init__(): Folder: {folder}, Alias: {alias}')
 
     def add(self, plant: Union[Plant, List[Plant], np.ndarray]):
         if isinstance(plant, Plant):
@@ -229,7 +226,7 @@ class Simulation:
             self.density_field_buffer.add(
                 field=self.density_field.values, t=self.t)
 
-    def run(self, T, min_population=None, max_population=None, transient_period=2, delta_p=0, convergence_stop=False):
+    def run(self, T, min_population=None, max_population=None, transient_period=0, delta_p=0, convergence_stop=False):
 
         start_time = time.time()
         n_iter = int(np.ceil(T / self.time_step))
@@ -240,7 +237,6 @@ class Simulation:
                 if _ > transient_period:
                     self.precipitation = max(0, self.precipitation + delta_p)
                 self.step()
-                is_converged, convergence_factor = self.convergence_check()[:2]
 
                 if self.verbose:
                     elapsed_time = time.time() - start_time
@@ -261,8 +257,7 @@ class Simulation:
                         'Time', 'Biomass', 'Population', 'Precipitation']].values.reshape(-1)
                     t = float(round(t, 2))
 
-                    print(f'{dots} Elapsed time: {elapsed_time_str}' + ' '*5 + f'|  {t=:^8}  |  N = {
-                          population:<6}  |  B = {np.round(biomass, 4):<6}  |  P = {np.round(precipitation, 6):<8}  |  conv = {np.round(convergence_factor, 8):<10}', end='\r')
+                    print(f'{dots} Elapsed time: {elapsed_time_str}' + ' '*5 + f'|  {t=:^8}  |  N = {population:<6}  |  B = {np.round(biomass, 4):<6}  |  P = {np.round(precipitation, 6):<8}', end='\r')
                     
                     if len(self.species_list) > 1:
                         if _ % 100 == 0:
@@ -399,41 +394,62 @@ class Simulation:
 
     def attempt_spawn(self, n, species_list=[]):
 
-        # Take care of the decimal part of n
+        # Take care of the decimal part of the spawn rate n
         decimal_part = n - int(n)
         n = int(n)
         if np.random.rand() < decimal_part:
             n += 1
 
+        # Generate new positions
         new_positions = np.random.uniform(-self.half_width,
                                           self.half_width, (n, 2))
+        new_species = np.random.choice(species_list, n)
         
-        new_plants = []
+        # Collect all positions that are not colliding with other plants
+        not_colliding_indices = []
         for i, pos in enumerate(new_positions):
-            current_species = np.random.choice(species_list)
+            current_species = new_species[i]
             
             # COLLISION CHECK
+            collisions = []
             if self.kt is not None:
                 indices_neighbours = self.kt.query_ball_point(pos, current_species.r_min+self.maximum_plant_size)
                 for j in indices_neighbours:
                     if (pos[0] - self.plants[j].x)**2 + (pos[1] - self.plants[j].y)**2 < (current_species.r_min + self.plants[j].r)**2:
-                        continue     
+                        collisions.append(j)
+            if len(collisions) == 0:
+                not_colliding_indices.append(i)
             
-            germination_chances = np.maximum(self.land_quality, self.local_density(pos) * self.precipitation * current_species.germination_chance)
-            random_values = np.random.uniform(0, 1, len(pos))
+        # Filter out the positions of the colliding plants
+        new_positions = new_positions[not_colliding_indices]
+        new_species = new_species[not_colliding_indices]
+        
+        
+        new_plants = []
+        if len(new_positions) > 0:
+            germination_chances = np.maximum(self.land_quality, self.local_density(new_positions)) * self.precipitation * current_species.germination_chance
+            random_values = np.random.uniform(0, 1, len(new_positions))
             spawn_indices = np.where(
                 germination_chances > random_values)[0]
+            
+            new_positions = new_positions[spawn_indices]
+            new_species = new_species[spawn_indices]
 
-            new_plants.append(
-                current_species.create_plant(
-                    id=self.id_generator.get_next_id(),
-                    x=pos[0],
-                    y=pos[1],
-                    r=current_species.r_min
+            for i, pos in enumerate(new_positions):
+                current_species = new_species[i]
+                new_plants.append(
+                    current_species.create_plant(
+                        id=self.id_generator.get_next_id(),
+                        x=pos[0],
+                        y=pos[1],
+                        r=current_species.r_min
+                    )
                 )
-            )
 
-        self.add(new_plants)
+        if len(new_plants) > 0:
+            self.add(new_plants)
+            print(f'Simulation.attempt_spawn(): {len(new_plants)} plants spawned.', end='\r')
+            print()
 
     def get_collisions(self):
         collisions = []
@@ -455,7 +471,13 @@ class Simulation:
         pos = np.atleast_2d(pos)
         pos_in_box = (np.abs(pos[:, 0]) < self.half_width) & (np.abs(pos[:, 1]) < self.half_height)
         return pos_in_box
-
+    
+    def pos_shift_periodic(self, pos):
+        pos = np.atleast_2d(pos)
+        pos[:, 0] = np.mod(pos[:, 0] + self.half_width, 2*self.half_width) - self.half_width
+        pos[:, 1] = np.mod(pos[:, 1] + self.half_height, 2*self.half_height) - self.half_height
+        return pos
+    
     def local_density(self, pos):
         # DO BOUNDARY CONDITIONS HERE
         return self.density_field.query(pos)
@@ -475,7 +497,7 @@ class Simulation:
                     
         new_positions = new_positions[~np.isnan(new_positions).any(axis=1)]
        
-        germination_chances = np.maximum(self.land_quality, self.local_density(new_positions) * self.precipitation * parent.germination_chance)
+        germination_chances = np.maximum(self.land_quality, self.local_density(new_positions)) * self.precipitation * parent.germination_chance
         random_values = np.random.uniform(0, 1, len(new_positions))
         germination_indices = np.where(germination_chances > random_values)[0]
 
@@ -536,7 +558,8 @@ class Simulation:
             )
 
         self.add(new_plants)
-        self.initiate()
+        self.update_kdtree(self.plants)
+        self.density_field.update(self.plants)
         self.data_buffer.add(data=self.collect_data())
         self.state_buffer.add(plants=self.plants, t=self.t)
         
@@ -581,7 +604,8 @@ class Simulation:
                 f"Simulation.spawn_non_overlapping(): Only {len(new_plants)} circles were placed after {max_attempts} attempts.")
 
         self.add(new_plants)
-        self.initiate()
+        self.update_kdtree(self.plants)
+        self.density_field.update(self.plants)
         self.data_buffer.add(data=self.collect_data())
         self.state_buffer.add(plants=self.plants, t=self.t)
 
@@ -619,7 +643,8 @@ class Simulation:
     #         for i, r in enumerate(samples)
     #     ]
     #     self.add(plants)
-    #     self.initiate()
+        # self.update_kdtree(self.plants)
+        # self.density_field.update(self.plants)
     #     self.data_buffer.add(data=self.collect_data())
     #     self.state_buffer.add(plants=state=self.plants, t=self.t)
 
@@ -653,7 +678,8 @@ class Simulation:
     #             )
 
     #     self.add(plants)
-    #     self.initiate()
+        # self.update_kdtree(self.plants)
+        # self.density_field.update(self.plants)
     exclude_default = [
         'alias',
         'biomass_buffer',

scripts/python/testing-linear_precipitation.py
@@ -11,46 +11,34 @@ from datetime import datetime
 seed = np.random.randint(0, 1_000_000_000)
 np.random.seed(seed)
 kwargs = {
-    'L': 1000,
-    'precipitation': 0.5,
+    'L': 500,
     'seed': seed,
 }
-num_plants = int(kwargs['L'])
 
 current_time = datetime.now().strftime("%y%m%d_%H%M%S")
-folder = f'Data/dynamics/recovery_rates'
-
-alias = f'rec_rate_test2'
-print(f'{seed = }')
+folder = f'Data/linear_precipitation/L{kwargs["L"]}'
+alias = f'lin_prec_L{kwargs["L"]}_temp'
 
 os.makedirs(folder, exist_ok=True)
 sim = Simulation(folder=folder, alias=alias, **kwargs, override=True)
+sim.precipitation = 0.3
 
-print(f'num_plants: {num_plants}')
+num_plants = int(kwargs['L'])
 sim.initiate_non_overlapping(n=num_plants, species_list=sim.species_list, max_attempts=50*num_plants)
 
-n_steps = 10
-T = 2000
-precipitation_step = - kwargs['precipitation'] / (n_steps - 1)
+T = 31000
+precipitation_step = - sim.precipitation / int(T - 1000)
 print(f'{precipitation_step = }')
-for i in range(n_steps):
-    print(f'Running step {i+1}/{n_steps}')
-    sim.run(T=T)
-    
-    # Remove a fraction of the plants
-    sim.remove_fraction(0.15)
-    
-    sim.run(T=T)
-    
-    sim.precipitation = sim.precipitation + precipitation_step
+
+sim.run(T=T, delta_p=precipitation_step)
     
 
 
 figs, axs, titles = sim.plot_buffers(title=alias)
 os.makedirs(folder + '/figures', exist_ok=True)
 for i, (fig, title) in enumerate(zip(figs, titles)):
-    tilte = title.replace(' ', '-').lower()
-    fig.savefig(f'{folder}/figures/{title}.png', dpi=1000)
+    title = title.replace(' ', '-').lower()
+    fig.savefig(f'{folder}/figures/{title}.png', dpi=600)
 
 # anim, _ = StateBuffer.animate(
 #     sim.state_buffer.get_data(), skip=10, title=alias, fast=True)